0dc61e4b2d93b0ab71625abef8b9817c12b9657f
Mohamed Awde <116069156+MohamedAwde@users.noreply.github.com>
2023-04-17 10:17:30 +0200

    client transaction overview

--- src/app/pages/inventory/pages/clients/pages/transactions/pages/overview/overview.component.ts
+++ src/app/pages/inventory/pages/clients/pages/transactions/pages/overview/overview.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { AgGridModule } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+import { ITransaction } from 'src/app/core/models/itransaction';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss'],
   standalone: true,
+  imports: [AgGridModule],
 })
-export class OverviewComponent {}
+export class OverviewComponent {
+  defaultColDef: ColDef = {
+    sortable: true,
+    flex: 1,
+  };
+
+  columnDefs: ColDef[] = [
+    { field: 'type', headerName: 'نوع العملية' },
+    { field: 'id', headerName: 'رقم العملية' },
+    { field: 'date', headerName: 'تاريخ العملية' },
+    { field: 'account', headerName: 'الحساب' },
+  ];
+
+  rowData: ITransaction[] | null = null;
+
+  public overlayLoadingTemplate =
+    '<span class="ag-overlay-loading-center">جاري تحميل البيانات</span>';
+  public overlayNoRowsTemplate =
+    '<span class="ag-overlay-loading-center">لا توجد اي معاملات حتي الان</span>';
+}
